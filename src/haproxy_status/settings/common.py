1388fb95ab3cfddbc4252f6ca2298445f535da3d
Fredrik Thulin <fredrik@thulin.net>
2018-12-13 17:23:11 +0100

    adjust HEALTHY_BACKEND_UPTIME

--- src/haproxy_status/settings/common.py
+++ src/haproxy_status/settings/common.py
@@ -2,5 +2,7 @@ DEBUG = True
 BACKEND_DIR = '/backends'
 STATS_URL = '/var/run/haproxy-control/stats'
 LOG_DOWN_INTERVAL = 60
-HEALTHY_BACKEND_UPTIME = 30
-FETCH_HAPROXY_STATUS_INTERVAL = 30
+FETCH_HAPROXY_STATUS_INTERVAL = 15
+# HEALTHY_BACKEND_UPTIME needs to be 2x FETCH_HAPROXY_STATUS_INTERVAL
+# to avoid flapping if a service really is restarting (also account for random fuzzing)
+HEALTHY_BACKEND_UPTIME = FETCH_HAPROXY_STATUS_INTERVAL * 2 + 2
