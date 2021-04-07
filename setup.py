176372dd68ba8228f9e67b35fa7d63179b040cd3
Fredrik Thulin <fredrik@thulin.net>
2021-04-07 10:40:09 +0200

    update requirements-file parsing to allow for hashes

--- setup.py
+++ setup.py
@@ -1,30 +1,41 @@
-from setuptools import setup, find_packages
 import os
+from pathlib import PurePath
+from typing import List
+
+from setuptools import find_packages, setup
 
 
-here = os.path.abspath(os.path.dirname(__file__))
+def load_requirements(path: PurePath) -> List[str]:
+    """ Load dependencies from a requirements.txt style file, ignoring comments etc. """
+    res = []
+    with open(path) as fd:
+        for line in fd.readlines():
+            while line.endswith('\n') or line.endswith('\\'):
+                line = line[:-1]
+            line = line.strip()
+            if not line or line.startswith('-') or line.startswith('#'):
+                continue
+            res += [line]
+    return res
+
+version = '0.0.3'
+
+here = PurePath(__file__)
+
+install_requires = load_requirements(here.with_name('requirements.txt'))
+test_requires = load_requirements(here.with_name('test_requirements.txt'))
+
 README = 'SUNET haproxy status'
-CHANGES = ''
 try:
     README = open(os.path.join(here, 'README.rst')).read()
 except IOError:
     pass
-try:
-    CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
-except IOError:
-    pass
-
-version = '0.0.3'
-
-install_requires = [x for x in open(os.path.join(here, 'requirements.txt')).read().split('\n') if len(x) > 0]
-testing_extras = [x for x in open(os.path.join(here, 'test_requirements.txt')).read().split('\n')
-                  if len(x) > 0 and not x.startswith('-')]
 
 setup(
     name='haproxy_status',
     version=version,
     description='SUNET haproxy status page',
-    long_description=README + '\n\n' + CHANGES,
+    long_description=README,
     # TODO: add classifiers
     classifiers=[
         # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
